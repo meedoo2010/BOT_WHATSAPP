f0bda45322e75b7482c560dfa0bd6aa7612b6534
meedoo2010 <meedookhaled72@gmail.com>
2025-12-22 02:32:04 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth(), // يحفظ تسجيل الدخول
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+    console.log('امسح QR من واتساب');
+});
+
+client.on('ready', () => {
+    console.log('✅ البوت اشتغل بنجاح');
+});
+
+client.on('message', message => {
+    const msg = message.body.toLowerCase();
+
+    if (msg === 'مرحبا') {
+        message.reply('أهلاً بيك 👋');
+    }
+
+    if (msg === 'سعر') {
+        message.reply('الأسعار تبدأ من 100 جنيه 💰');
+    }
+
+    if (msg === 'سلام') {
+        message.reply('مع السلامة ❤️');
+    }
+});
+
+client.initialize();
