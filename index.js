5b9034b62389febfc81db8d479835651bcf37b51
meedoo2010 <meedookhaled72@gmail.com>
2025-12-26 16:51:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,37 +2,36 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-authStrategy: new LocalAuth(), // يحفظ تسجيل الدخول
-puppeteer: {
-headless: true,
-args: ['--no-sandbox', '--disable-setuid-sandbox']
-}
+    authStrategy: new LocalAuth({clientId: "bot1"}), // يحفظ تسجيل الدخول
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
 
 client.on('qr', qr => {
-qrcode.generate(qr, { small: true });
-console.log('From WhatsApp, scan the QR code');
+    qrcode.generate(qr, { small: true });
+    console.log('امسح QR من واتساب');
 });
 
 client.on('ready', () => {
-console.log('✅ The bot worked successfully');
+    console.log('✅ البوت اشتغل بنجاح');
 });
 
 client.on('message', message => {
-const msg = message.body.toLowerCase();
+    const msg = message.body.toLowerCase();
 
-if (msg === 'مرحبا') {  
-    message.reply('أهلاً بيك 👋');  
-}  
+    if (msg === 'مرحبا') {
+        message.reply('أهلاً بيك 👋');
+    }
 
-if (msg === 'اسعار') {  
-    message.reply('أسعار مواقع تبدأ من 200 جنيه, التطبيقات تبدأ من 150,API يبدأ من 300  💰');  
-}  
+    if (msg === 'اسعار') {
+        message.reply('أسعار مواقع تبدأ من 500 جنيه, التطبيقات تبدأ من 400 جنيه, API تبدأ من 600 جنيه 💰');
+    }
 
-if (msg === 'سلام') {  
-    message.reply('مع السلامة ❤️');  
-}
-
+    if (msg === 'سلام') {
+        message.reply('مع السلامة ❤️');
+    }
 });
 
 client.initialize();
